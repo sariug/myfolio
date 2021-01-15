518cc2b506514d74d2502b2fc8b64d973f37e89e
sariug <ugurcansari93@gmail.com>
2021-01-15 21:52:51 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { RouterModule } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    ResumeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path:'', component: HomeComponent },
+      { path: 'resume', component:ResumeComponent},
+      { path: 'projects', component:ProjectsComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
